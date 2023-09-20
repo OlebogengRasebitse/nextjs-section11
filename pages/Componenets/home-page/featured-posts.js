f07433f9315b7f383d14df065375f1f24305fb84
OlebogengRasebitse <patiencerasebitse@gmail.com>
2023-09-20 16:27:16 +0200

    Created dummy data(DUMMY_POSTS).

--- pages/Componenets/home-page/featured-posts.js
+++ pages/Componenets/home-page/featured-posts.js
@@ -1,9 +1,11 @@
+import PostsGrid from '../posts/posts-grid';
 import classes from './featured-posts.module.css';
 
-function FeaturedPosts() {
+function FeaturedPosts(prop) {
     return ( 
         <section className={className.latest}> 
         <h2>Featured Posts</h2> 
+        <PostsGrid posts={propagateServerField.posts}/>
         </section>
      );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,40 @@
 import { Fragment } from "react";
 import Hero from "./Componenets/home-page/hero";
+import FeaturedPosts from "./Componenets/home-page/featured-posts";
+
+const DUMMY_POSTS = [
+    {  slug: 'getting-started-with-nextjs',
+         title: 'Getting-Started-With-NextJS', 
+    image: 'getting-started-nextjs.png', 
+    excerpt: 'NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
+     date: '2022-02-10', 
+    },
+    {  slug: 'getting-started-with-nextjs2',
+    title: 'Getting-Started-With-NextJS', 
+image: 'getting-started-nextjs.png', 
+excerpt: 'NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
+date: '2022-02-10', 
+},
+{  slug: 'getting-started-with-nextjs3',
+title: 'Getting-Started-With-NextJS', 
+image: 'getting-started-nextjs.png', 
+excerpt: 'NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
+date: '2022-02-10', 
+},
+{  slug: 'getting-started-with-nextjs4',
+title: 'Getting-Started-With-NextJS', 
+image: 'serenity.png', 
+excerpt: 'NextJS is the React framework for production, it makes building fullstack React apps and sites a breeze and ships with built-in server-side rendering.',
+date: '2022-02-10', 
+},
+];
+
 
 function HomePage() {
     return ( 
         <Fragment> 
             <Hero />
+            <FeaturedPosts posts={DUMMY_POSTS} />
         </Fragment>
      );
 }
